0dbfcfc329a90951e3e12d1bef9fef22f6fe8c3b
jay1261 <dongjae73@naver.com>
2025-05-12 16:36:59 +0900

    [Add] howtoeat 백엔드 파일 추가

---
+++ src/main/java/com/daall/howtoeat/HowToEatApplication.java
@@ -0,0 +1,14 @@
+package com.daall.howtoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowToEatApplication.class, args);
+		System.out.println("test");
+	}
+
+}
