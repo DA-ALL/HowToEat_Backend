ce8d926bacf42eae39eea5831ee89f617a87fb85
YeahyunKim <insidesy4@gmail.com>
2025-05-13 17:40:57 +0900

    [Feat] notices 테이블 엔티티 생성

--- src/main/java/com/daall/howtoeat/domain/notice/Notice.java
+++ src/main/java/com/daall/howtoeat/domain/notice/Notice.java
@@ -1,4 +1,26 @@
 package com.daall.howtoeat.domain.notice;
 
-public class Notice {
+import com.daall.howtoeat.common.Timestamped;
+import com.daall.howtoeat.common.enums.NoticeType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "notices")
+public class Notice extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "Text", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private NoticeType noticeType;
 }
