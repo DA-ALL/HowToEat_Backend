b18c18fdd6ab28bc640d1868fb582c7040448325
YeahyunKim <insidesy4@gmail.com>
2025-05-13 15:18:56 +0900

    [Feat] user_targets 테이블 생성

---
+++ src/main/java/com/daall/howtoeat/common/enums/UserActivityLevel.java
@@ -0,0 +1,15 @@
+package com.daall.howtoeat.common.enums;
+
+public enum UserActivityLevel {
+    VERY_HIGH(1.9),
+    HIGH(1.725),
+    NORMAL(1.55),
+    LOW(1.375),
+    VERY_LOW(1.2);
+
+    private final Double kcalOffset;
+
+    UserGoal(Double amount) {
+        this.kcalOffset = amount;
+    }
+}
---
+++ src/main/java/com/daall/howtoeat/user/UserTargets.java
@@ -0,0 +1,44 @@
+package com.daall.howtoeat.user;
+
+import com.daall.howtoeat.common.Timestamped;
+import com.daall.howtoeat.common.enums.Gender;
+import com.daall.howtoeat.common.enums.UserActivityLevel;
+import com.daall.howtoeat.common.enums.UserGoal;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user_targets")
+public class UserTargets extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Integer kcal;
+
+    @Column(nullable = false)
+    private Double carbo;
+
+    @Column(nullable = false)
+    private Double protein;
+
+    @Column(nullable = false)
+    private Double fat;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserGoal goal;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserActivityLevel activityLevel;
+
+}
