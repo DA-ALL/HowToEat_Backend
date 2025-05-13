bf8db2b2354bf66d3964418383d4344fe8d6a43d
YeahyunKim <insidesy4@gmail.com>
2025-05-13 17:59:51 +0900

    [Feat] recommend_foods 테이블 엔티티 생성

---
+++ src/main/java/com/daall/howtoeat/domain/food/RecommendFood.java
@@ -0,0 +1,20 @@
+package com.daall.howtoeat.domain.food;
+
+import com.daall.howtoeat.common.Timestamped;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "recommend_foods")
+public class RecommendFood extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_id", nullable = false)
+    private Food food;
+}
