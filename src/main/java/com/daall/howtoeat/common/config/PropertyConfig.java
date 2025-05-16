c93721eb1a230690ea96fe825c49579057779c93
jay1261 <dongjae73@naver.com>
2025-05-16 15:33:21 +0900

    [Feat] SecurityConfig 추가

--- src/main/java/com/daall/howtoeat/common/config/PropertyConfig.java
+++ src/main/java/com/daall/howtoeat/common/config/PropertyConfig.java
@@ -1,4 +1,4 @@
-package com.daall.howtoeat.common;
+package com.daall.howtoeat.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
