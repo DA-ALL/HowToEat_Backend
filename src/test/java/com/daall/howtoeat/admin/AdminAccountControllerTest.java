b9bcd54108a214a4cb67e8fa3fe8bb0848f8c01e
jay1261 <dongjae73@naver.com>
2025-05-27 17:58:03 +0900

    [Feat] 관리자 계정 생성 테스트코드 #3

--- src/test/java/com/daall/howtoeat/admin/AdminAccountControllerTest.java
+++ src/test/java/com/daall/howtoeat/admin/AdminAccountControllerTest.java
@@ -1,4 +1,33 @@
+package com.daall.howtoeat.admin;
+
+import com.daall.howtoeat.common.enums.UserRole;
+import com.daall.howtoeat.domain.user.User;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 class AdminAccountControllerTest {
-  
+    @Autowired
+    AdminAccountService adminAccountService;
+
+    @Test
+    void createAdminMaster(){
+        AdminAccountRequestDto mock1 = Mockito.mock(AdminAccountRequestDto.class);
+
+        when(mock1.getAccountId()).thenReturn("admin");
+        when(mock1.getPassword()).thenReturn("1234");
+
+        User userMock = Mockito.mock(User.class);
+        when(userMock.getId()).thenReturn(1L);
+        when(userMock.getUserRole()).thenReturn(UserRole.MASTER);
+        when(userMock.getBirth()).thenReturn(LocalDate.now());
+        when(userMock.getName()).thenReturn("admin");
+
+        adminAccountService.createAdminAccount(new User(), mock1);
+    }
 }
