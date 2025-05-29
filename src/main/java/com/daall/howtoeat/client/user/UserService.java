d04ea96020dda95506d72f00036e9c934f5d303b
jay1261 <dongjae73@naver.com>
2025-05-29 18:10:53 +0900

    [Refact] usercontroller 테스트 코드 삭제 및 리팩토링 #3

--- src/main/java/com/daall/howtoeat/client/user/UserService.java
+++ src/main/java/com/daall/howtoeat/client/user/UserService.java
@@ -3,6 +3,7 @@ package com.daall.howtoeat.client.user;
 import com.daall.howtoeat.client.user.dto.SignupRequestDto;
 import com.daall.howtoeat.domain.user.User;
 import com.daall.howtoeat.domain.user.UserStat;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserTargetService userTargetService;
 
+    @Transactional
     public User signup(SignupRequestDto requestDto) {
         if (userRepository.existsByEmail(requestDto.getEmail())) {
             throw new IllegalArgumentException("이미 가입된 이메일입니다.");
@@ -26,10 +28,6 @@ public class UserService {
 
         UserStat userStats = new UserStat(savedUser, requestDto);
         return user;
-    }
-
-    public void test() {
-        System.out.println("test입니당~");
     }
 
     public void updateRefreshToken(User user, String refreshToken) {
