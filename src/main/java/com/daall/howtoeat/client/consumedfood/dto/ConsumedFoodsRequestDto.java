46ce1d11b17c975533f911fd515bcbd9e417b05b
YeahyunKim <insidesy4@gmail.com>
2025-06-09 14:48:56 +0900

    [Add] 음식 기록을 위한 ConsumedFoodsRequestDto 파일 추가 #11

---
+++ src/main/java/com/daall/howtoeat/client/consumedfood/dto/ConsumedFoodsRequestDto.java
@@ -0,0 +1,46 @@
+package com.daall.howtoeat.client.consumedfood.dto;
+
+import com.daall.howtoeat.common.enums.FoodType;
+import com.daall.howtoeat.common.enums.MealTime;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class ConsumedFoodsRequestDto {
+    @NotNull(message = "음식 코드가 비어있습니다. 다시 시도해주세요.")
+    private String foodCode;
+
+    @NotNull(message = "음식 이름이 비어있습니다. 다시 시도해주세요.")
+    private String foodName;
+
+    @NotNull(message = "식사 시간이 비어있습니다. 다시 시도해주세요.")
+    private MealTime mealTime;
+
+    @NotNull(message = "음식 중량이 비어있습니다. 다시 시도해주세요.")
+    private Double foodWeight;
+
+    @NotNull(message = "음식 종류가 비어있습니다. 다시 시도해주세요.")
+    private FoodType foodType;
+
+    @NotNull(message = "칼로리 정보가 비어있습니다. 다시 시도해주세요.")
+    private Double kcal;
+
+    @NotNull(message = "탄수화물 정보가 비어있습니다. 다시 시도해주세요.")
+    private Double carbo;
+
+    @NotNull(message = "단백질 정보가 비어있습니다. 다시 시도해주세요.")
+    private Double protein;
+
+    @NotNull(message = "지방 정보가 비어있습니다. 다시 시도해주세요.")
+    private Double fat;
+
+    @NotNull(message = "제공처 정보가 비어있습니다. 다시 시도해주세요.")
+    private String providedBy;
+
+    @NotNull(message = "1회 제공량 여부 정보가 비어있습니다. 다시 시도해주세요.")
+    private Boolean isPerServing;
+
+    @NotNull(message = "단위 정보가 비어있습니다. 다시 시도해주세요.")
+    private String unit;
+
+}
