447163c6961c0455483df7004b4e763fc7b9aaef
YeahyunKim <insidesy4@gmail.com>
2025-07-25 16:03:30 +0900

    [Feat] 애플 로그인 구현 #78

---
+++ src/main/java/com/daall/howtoeat/common/security/AppleKeyUtil.java
@@ -0,0 +1,25 @@
+package com.daall.howtoeat.common.security;
+
+import java.security.KeyFactory;
+import java.security.interfaces.ECPrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+
+public class AppleKeyUtil {
+
+    public static ECPrivateKey getPrivateKeyFromString(String keyString) {
+        try {
+            String privateKeyPEM = keyString
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "")
+                    .replaceAll("\\s+", "");
+
+            byte[] keyBytes = Base64.getDecoder().decode(privateKeyPEM);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("EC"); // 애플 키는 ECDSA 기반
+            return (ECPrivateKey) keyFactory.generatePrivate(keySpec);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid Apple Private Key", e);
+        }
+    }
+}
