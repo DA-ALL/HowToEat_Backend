413a537d9b68a16ac87b4117f15ff9f24e5f580e
jay1261 <dongjae73@naver.com>
2025-07-27 18:09:41 +0900

    [Fix] 코드 정리 #78

--- src/main/java/com/daall/howtoeat/common/security/AppleClientSecretInitializer.java
+++ src/main/java/com/daall/howtoeat/common/security/AppleClientSecretInitializer.java
@@ -23,15 +23,10 @@ public class AppleClientSecretInitializer {
 
     @PostConstruct
     public void setAppleClientSecret() {
-
-        System.out.println("클라이언트 시크릿 만들기 시작");
         String clientSecret = AppleClientSecretProvider.createClientSecret(
                 teamId, clientId, keyId, privateKey
         );
 
         System.setProperty("APPLE_CLIENT_SECRET", clientSecret);
-
-        System.out.println("[AppleClientSecretInitializer] 생성된 client_secret:");
-        System.out.println(clientSecret);
     }
 }
