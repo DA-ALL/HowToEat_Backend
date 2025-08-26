f9decd1b652edd4046f72646781c344c9128d460
username <insidesy4@gmail.com>
2025-08-26 16:14:58 +0900

    [Feat] @GetMapping(/users/meal-schedules) 유저 식사 시간 조회 API 작업 진행 #118

--- src/main/java/com/daall/howtoeat/client/usesrmealschedule/UserMealScheduleRepository.java
+++ src/main/java/com/daall/howtoeat/client/usesrmealschedule/UserMealScheduleRepository.java
@@ -5,8 +5,13 @@ import com.daall.howtoeat.domain.user.UserMealSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserMealScheduleRepository extends JpaRepository<UserMealSchedule, Long> {
+    //유저로 조회하기
+    Optional<UserMealSchedule> findByUser(User user);
+
     //회원탈퇴 시 연관관계 끊기
     void deleteAllByUser(User user);
 }
--- src/main/java/com/daall/howtoeat/client/usesrmealschedule/UserMealScheduleService.java
+++ src/main/java/com/daall/howtoeat/client/usesrmealschedule/UserMealScheduleService.java
@@ -1,6 +1,9 @@
 package com.daall.howtoeat.client.usesrmealschedule;
 
 import com.daall.howtoeat.client.user.dto.SignupRequestDto;
+import com.daall.howtoeat.client.usesrmealschedule.dto.UserMealScheduleResponseDto;
+import com.daall.howtoeat.common.enums.ErrorType;
+import com.daall.howtoeat.common.exception.CustomException;
 import com.daall.howtoeat.domain.user.User;
 import com.daall.howtoeat.domain.user.UserMealSchedule;
 import lombok.RequiredArgsConstructor;
@@ -15,5 +18,11 @@ public class UserMealScheduleService {
         UserMealSchedule userMealSchedule = new UserMealSchedule(user, requestDto);
 
         userMealScheduleRepository.save(userMealSchedule);
+    }
+
+    public UserMealScheduleResponseDto getUserMealSchedule(User loginUser) {
+        UserMealSchedule userMealSchedule = userMealScheduleRepository.findByUser(loginUser).orElseThrow(() -> new CustomException(ErrorType.NOT_FOUND_USER_MEAL_SCHEDULE));
+
+        return new UserMealScheduleResponseDto(userMealSchedule);
     }
 }
